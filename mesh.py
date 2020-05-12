c31d360cc2e2128ec13aca526215759cfae67cef
sergio <sergio@priveapp.com>
2020-05-12 04:00:34 -0700

    hd dolly-zoom-in

--- mesh.py
+++ mesh.py
@@ -2289,7 +2289,10 @@ def output_3d_photo(verts, colors, faces, Height, Width, hFov, vFov, tgt_poses, 
         clip = ImageSequenceClip(stereos, fps=config['fps'])
         if isinstance(video_basename, list):
             video_basename = video_basename[0]
-        clip.write_videofile(os.path.join(output_dir, video_basename + '_' + video_traj_type + '.mp4'), fps=config['fps'])
+        output_file_path = os.path.join(output_dir, video_basename + '_' + video_traj_type + '.mp4')
+        clip.write_videofile(output_file_path,
+                             fps=config['fps'],
+                             ffmpeg_params=['-x264opts', 'keyint=1'])
 
 
 
